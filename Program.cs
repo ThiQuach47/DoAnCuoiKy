4349c7a300705503a96d1568a4071ad44cab9317
ThiQuach47 <78086280+ThiQuach47@users.noreply.github.com>
2021-01-27 20:10:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿using DoAn;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todolist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Application application = new Application();
+            application.ShowMenu();
+
+            //Console.ReadKey();
+
+        }
+    }
+
+
+    class Application
+    {
+        List<Work> Lists;
+
+        public Application()
+        {
+            Lists = new List<Work>();
+        }
+        public void ShowMenu()
+        {
+            do
+            {
+                Console.WriteLine("============= MENU ===========");
+                Console.WriteLine("1. Thêm công việc.");
+                Console.WriteLine("2. Xem danh sách công việc.");
+                Console.WriteLine("3. Đánh dấu đã hoàn thành công việc.");
+                Console.WriteLine("4. Xoá công việc.");
+                Console.WriteLine("0. Thoát chương trình.");
+                Console.Write("Mời nhập lựa chọn : ");
+                int luachon = getChoses();
+
+                switch (luachon)
+                {
+                    case 1:
+                        {
+                            TaoCV();
+                            break;
+                        }
+                    case 2:
+                        {
+                            HienThiDS();
+                            break;
+                        }
+                    case 3:
+                        {
+                            HoanThanhCV();
+                            break;
+                        }
+                    case 4:
+                        {
+                            XoaCV();
+                            break;
+                        }
+                    case 0:
+                        {
+                            Console.WriteLine("Hẹn gặp lại lần sau.");
+                            return;
+                        }
+                    default:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                            break;
+                        }
+                }
+            } while (true);
+        }
+
+        private void XoaCV()
+        {
+        }
+
+        public void HoanThanhCV()
+        {
+        }
+
+        private void SapXep()
+        {
+          
+            Lists.Sort(delegate (Work x, Work y)
+            {
+                if (x.GetIsSuccess() == y.GetIsSuccess())
+                {
+                    return x.GetTimeDue().CompareTo(y.GetTimeDue());
+                }
+                else
+                {
+                    if (x.GetIsSuccess() == false)
+                    {
+                        return -1;
+                    }
+                    return 1;
+                }
+            }
+            );
+        }
+
+
+
+        public void TaoCV()
+        {
+            Console.Clear();
+            Work work = new Work();
+            work.Input();
+
+            Console.Clear();
+            work.Ouput();
+            Console.Write("(Ấn 1 phím bất kỳ để quay lại Menu)");
+
+            Lists.Add(work);
+            SapXep();
+
+            Console.ReadKey();
+            Console.Clear();
+            
+        }
+
+        public void HienThiDS()
+        {
+            Console.Clear();
+            Console.WriteLine("========== Danh sách công việc ========");
+            Console.WriteLine("{0, -20}|{1, -35}|{2, -15}|{3, -15}|{4, -17}", "Tên công việc", "Nội dung", "Ngày tạo", "Ngày hết hạn", "Hoàn thành");
+            Console.WriteLine(new String('=', 102));
+            for (int i = 0; i < Lists.Count; i++)
+            {
+                Console.WriteLine(Lists[i].ToString());
+            }
+            Console.Write("(Ấn 1 phím bất kỳ để quay lại Menu)");
+
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+        public int getChoses()
+        {
+            try
+            {
+                return int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
+
+
+   
---
+++ Work.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn
+{
+    class Work
+    {
+        private string name;
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string value)
+        {
+            name = value;
+        }
+
+        private string content;
+
+        public string GetContent()
+        {
+            return content;
+        }
+
+        public void SetContent(string value)
+        {
+            content = value;
+        }
+
+        private DateTime timeCreate;
+
+        public DateTime GetTimeCreate()
+        {
+            return timeCreate;
+        }
+
+        public void SetTimeCreate(DateTime value)
+        {
+            timeCreate = value;
+        }
+
+        private DateTime timeDue;
+
+        public DateTime GetTimeDue()
+        {
+            return timeDue;
+        }
+
+        public void SetTimeDue(DateTime value)
+        {
+            timeDue = value;
+        }
+
+        private bool isSuccess;
+
+        public bool GetIsSuccess()
+        {
+            return isSuccess;
+        }
+
+        public void SetIsSuccess(bool value)
+        {
+            isSuccess = value;
+        }
+
+        public Work() { }
+
+        public Work(string name, string content, DateTime timeCreate)
+        {
+            SetName(name);
+            SetContent(content);
+            SetTimeCreate(timeCreate);
+            this.SetIsSuccess(false);
+        }
+
+        public void Input()
+        {
+            Console.Write("Nhập vào tên công việc : ");
+            SetName(Console.ReadLine());
+
+            Console.Write("Nhập vào nội dung công việc : ");
+            SetContent(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Nhập vào ngày hết hạn (d/M/YYYY): ");
+                string strTemp = Console.ReadLine();
+
+                try
+                {
+                    SetTimeDue(DateTime.ParseExact(strTemp, "d/M/yyyy", CultureInfo.CurrentCulture));
+                    if (GetTimeDue().Date.CompareTo(DateTime.Today) < 0)
+                    {
+                        Console.WriteLine("Ngày đã nhập không hợp lệ, vui lòng nhập lại.");
+                        continue;
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ngày đã nhập không hợp lệ, vui lòng nhập lại.");
+                }
+
+            } while (true);
+
+            SetTimeCreate(DateTime.Now);
+        }
+
+        public void Ouput()
+        {
+            Console.WriteLine("========= Công việc đã nhập ===========");
+            Console.WriteLine($"Tên công việc : {GetName()}");
+            Console.WriteLine($"Nội dung công việc : {GetContent()}");
+            Console.WriteLine($"Ngày tạo công việc : {GetTimeCreate().ToString("d/M/yyyy")}");
+            Console.WriteLine($"Ngày hết hạn : {GetTimeDue().ToString("d/M/yyyy")}");
+        }
+
+        public void setAccomplished()
+        {
+            SetIsSuccess(true);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0, -20}|{1, -35}|{2, -15}|{3, -15}|{4, -17}", GetName(), GetContent(), GetTimeCreate().ToString("d/M/yyyy"), GetTimeDue().ToString("d/M/yyyy"), (GetIsSuccess() ? "Đã hoàn thành" : "Chưa hoàn thành"));
+        }
+    }
+    
+}
+    
+
+    
+
